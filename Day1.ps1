012a3e3fbf973f4f31bef99681cb121f674ccd43
Gavin Eke <GavinEke@users.noreply.github.com>
2019-12-04 10:17:17 +1100

    Day1 Part2

--- Day1.ps1
+++ Day1.ps1
@@ -1,8 +1,30 @@
-﻿$inputfile = Get-Content 'AoC-Day1-Input.txt'
+<# Part 1
+#$inputfile = 100756
+$inputfile = Get-Content 'AoC-Day1-Input.txt'
 $totalfuel = 0
 
 ForEach ($Item in $inputfile) {
     $totalfuel = $totalfuel + ([math]::floor($Item/3)-2)
+}
+
+$totalfuel
+#>
+
+# Part 2
+#$inputfile = 100756
+$inputfile = Get-Content 'AoC-Day1-Input.txt'
+$totalfuel = 0
+
+ForEach ($Item in $inputfile) {
+    $modulefuel = ([math]::floor($Item/3)-2)
+    $moduletotal = $modulefuel
+
+    do {
+        $modulefuel = ([math]::floor($modulefuel/3)-2)
+        $moduletotal = $moduletotal + $modulefuel
+    } until ($modulefuel -le 5)
+
+    $totalfuel = $totalfuel + $moduletotal
 }
 
 $totalfuel
