970b3aa3495f486cf4089aff419dd866071d4434
Gavin Eke <GavinEke@users.noreply.github.com>
2019-12-04 14:02:37 +1100

    minor fix of input

--- Day2.ps1
+++ Day2.ps1
@@ -1,8 +1,9 @@
-﻿# Part 1
+# Part 1
 $i = 0
-$x = Get-Content 'AoC-Day2-Input.txt'
+$x = 1,9,10,3,2,3,11,0,99,30,40,50 #Replace with your puzzle input
 $x[1] = 12
 $x[2] = 2
+
 ForEach ($Item in $x) {
     if (0 -eq $i % 4) {
         if ($Item -eq 1) {
