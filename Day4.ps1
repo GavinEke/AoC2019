de4b0e4e7f71a700e72f4868f4229d43d5583846
Gavin Eke <GavinEke@users.noreply.github.com>
2019-12-05 09:51:16 +1100

    Day 4 - Part 1

---
+++ Day4.ps1
@@ -0,0 +1,30 @@
+﻿# Part 1
+$UniquePWCount = 0
+
+ForEach ($Item in 156218..652527) { # Change the numbers to your own input
+    $CharArray = ([string]$Item).ToCharArray()
+    $i = 0
+    $testcount = 0
+
+    while ($i -lt 6) {
+        if ($CharArray[$i] -le $CharArray[($i+1)]) {
+            $testcount++
+        }
+        $i++
+    }
+
+    if ($testcount -eq 5) {
+        $i = 0
+        while ($i -lt 6) {
+            if ($CharArray[$i] -eq $CharArray[($i+1)]) {
+                if ($lastsuccess -ne [int](-join $CharArray)) {
+                    $UniquePWCount++
+                    $lastsuccess = [int](-join $CharArray)
+                }
+            }
+            $i++
+        }
+    }
+}
+
+$UniquePWCount
