72259d8fbed48189b7c2b7b7f7391bc5b4419a87
Gavin Eke <GavinEke@users.noreply.github.com>
2019-12-05 10:23:04 +1100

    Day 4 - Part 2

--- Day4.ps1
+++ Day4.ps1
@@ -1,4 +1,4 @@
-﻿# Part 1
+<# Part 1
 $UniquePWCount = 0
 
 ForEach ($Item in 156218..652527) { # Change the numbers to your own input
@@ -24,6 +24,48 @@ ForEach ($Item in 156218..652527) { # Change the numbers to your own input
             }
             $i++
         }
+    }
+}
+
+$UniquePWCount
+#>
+
+# Part 2
+$PWList = New-Object System.Collections.ArrayList
+
+ForEach ($Item in 156218..652527) {
+    $CharArray = ([string]$Item).ToCharArray()
+    $i = 0
+    $testcount = 0
+
+    while ($i -lt 6) {
+        if ($CharArray[$i] -le $CharArray[($i+1)]) {
+            $testcount++
+        }
+        $i++
+    }
+
+    if ($testcount -eq 5) {
+        $i = 0
+        while ($i -lt 6) {
+            if ($CharArray[$i] -eq $CharArray[($i+1)]) {
+                if ($lastsuccess -ne [int](-join $CharArray)) {
+                    $lastsuccess = [int](-join $CharArray)
+                    $PWList.Add($lastsuccess)
+                }
+            }
+            $i++
+        }
+    }
+}
+
+$UniquePWCount = 0
+
+ForEach ($Item in $PWList) {
+    $CharArray = ([string]$Item).ToCharArray()
+
+    if (($CharArray | Group-Object | Select-Object -ExpandProperty Count) -eq 2) {
+        $UniquePWCount++
     }
 }
 
